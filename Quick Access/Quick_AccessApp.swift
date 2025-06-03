afda2fb0695ad7d6f02bae3b918713ef728b007a
hashbrun444 <karmen.matache9@gmail.com>
2025-06-02 18:40:43 -1000

    Initial Commit

---
+++ Quick Access/Quick_AccessApp.swift
@@ -0,0 +1,32 @@
+//
+//  Quick_AccessApp.swift
+//  Quick Access
+//
+//  Created by Cristian Matache on 6/2/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Quick_AccessApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Quick AccessUITests/Quick_AccessUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Quick_AccessUITestsLaunchTests.swift
+//  Quick AccessUITests
+//
+//  Created by Cristian Matache on 6/2/25.
+//
+
+import XCTest
+
+final class Quick_AccessUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
